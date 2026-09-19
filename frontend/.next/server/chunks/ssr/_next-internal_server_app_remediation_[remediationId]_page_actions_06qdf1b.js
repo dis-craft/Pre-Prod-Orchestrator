25087c5ca68a55bba85ef19c0de883c53aa456ce
sachinskyte <sachinpatel09628@gmail.com>
2026-09-19 10:56:59 +0530

    Remove tracked .next build cache artifacts from git

--- frontend/.next/server/chunks/ssr/_next-internal_server_app_remediation_[remediationId]_page_actions_06qdf1b.js
+++
@@ -1,3 +0,0 @@
-module.exports=[22241,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_remediation_%5BremediationId%5D_page_actions_06qdf1b.js.map
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"b3o2blhq2jrbcSY1rr7lR2rUkySnBrtsn4OZFIctfZ0=\"\n}"
